57096bd79761e1337e1e4ae3141512b11831afa0
shannon8611 <shannon8611@yahoo.com>
2017-12-05 20:32:47 -0600

    Add custom jquery code for button text change

---
+++ js/main.js
@@ -0,0 +1,7 @@
+//jQuery Codes
+
+$(document).ready(function() {
+	// Custom jQuery code where the element's content changes when the button is clicked
+	$('.next').click(function(){
+		$('.yes').text('Then what are you waiting for?! Contact Us.');
+	});
