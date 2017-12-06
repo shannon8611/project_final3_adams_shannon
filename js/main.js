e73f65a9acc6405db97f2502324d7a5dc34b4b27
shannon8611 <shannon8611@yahoo.com>
2017-12-05 20:33:54 -0600

    Add plugin for morphext

--- js/main.js
+++ js/main.js
@@ -5,3 +5,11 @@ $(document).ready(function() {
 	$('.next').click(function(){
 		$('.yes').text('Then what are you waiting for?! Contact Us.');
 	});
+	// This plugin allows different phrases to change in between a string
+	$("#js-rotating").Morphext({
+	    animation: "bounceIn",
+	    separator: ",",
+	    speed: 2000,
+	    complete: function () {
+	    }
+	});
